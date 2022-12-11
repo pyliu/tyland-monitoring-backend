b0fc7de38ef30b885df736a5c5c8f9f536b443aa
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-12 00:37:53 +0800

    Update general.js
    use utils.registerWorker

--- model/api/v1/systeminformation/general.js
+++ model/api/v1/systeminformation/general.js
@@ -4,20 +4,11 @@ const utils = require(path.join(__dirname, "..", "..", "..", "utils"));
 const StatusCodes = require("http-status-codes").StatusCodes;
 const { Worker } = require("worker_threads");
 
-const subscribe = function (res, worker, params = {}) {
-  // listen to message to wait response from worker
-  worker?.on("message", (data) => {
-    res.status(data.statusCode === config.statusCode.FAIL ? StatusCodes.NOT_ACCEPTABLE : StatusCodes.OK).send({ ...data });
-  });
-  // post data to worker thread
-  worker?.postMessage(params);
-}
-
 module.exports.register = (app) => {
   app.get(`/${config.apiPrefix}/v1/general/static`, (req, res) => {
     if (utils.authenticate(req.headers.authorization)) {
       const worker = new Worker(path.join(__dirname, '..', '..', '..', 'workers', 'v1', 'systeminformation', 'general', 'static.js'));
-      subscribe(res, worker);
+      utils.registerWorker(res, worker);
     } else {
       utils.badRequest(req, "❌ 認證失敗");
     }
