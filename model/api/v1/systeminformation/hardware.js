85c3a03acdbab7b7c2330337d9bc31ad53946346
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-12 00:38:38 +0800

    Update hardware.js
    use utils.registerWorker

--- model/api/v1/systeminformation/hardware.js
+++ model/api/v1/systeminformation/hardware.js
@@ -4,20 +4,11 @@ const utils = require(path.join(__dirname, "..", "..", "..", "utils"));
 const StatusCodes = require("http-status-codes").StatusCodes;
 const { Worker } = require("worker_threads");
 
-const subscribe = function (res, worker, params = {}) {
-  // listen to message to wait response from worker
-  worker?.on("message", (data) => {
-    res.status(data.statusCode === config.statusCode.FAIL ? StatusCodes.NOT_ACCEPTABLE : StatusCodes.OK).send({ ...data });
-  });
-  // post data to worker thread
-  worker?.postMessage(params);
-}
-
 module.exports.register = (app) => {
   app.get(`/${config.apiPrefix}/v1/hardware`, (req, res) => {
     if (utils.authenticate(req.headers.authorization)) {
       const worker = new Worker(path.join(__dirname, '..', '..', '..', 'workers', 'v1', 'systeminformation', 'hardware', 'default.js'));
-      subscribe(res, worker);
+      utils.registerWorker(res, worker);
     } else {
       utils.badRequest(req, "❌ 認證失敗");
     }
@@ -25,7 +16,7 @@ module.exports.register = (app) => {
   app.get(`/${config.apiPrefix}/v1/hardware/uuid`, (req, res) => {
     if (utils.authenticate(req.headers.authorization)) {
       const worker = new Worker(path.join(__dirname, '..', '..', '..', 'workers', 'v1', 'systeminformation', 'hardware', 'uuid.js'));
-      subscribe(res, worker);
+      utils.registerWorker(res, worker);
     } else {
       utils.badRequest(req, "❌ 認證失敗");
     }
@@ -33,7 +24,7 @@ module.exports.register = (app) => {
   app.get(`/${config.apiPrefix}/v1/hardware/bios`, (req, res) => {
     if (utils.authenticate(req.headers.authorization)) {
       const worker = new Worker(path.join(__dirname, '..', '..', '..', 'workers', 'v1', 'systeminformation', 'hardware', 'bios.js'));
-      subscribe(res, worker);
+      utils.registerWorker(res, worker);
     } else {
       utils.badRequest(req, "❌ 認證失敗");
     }
@@ -41,7 +32,7 @@ module.exports.register = (app) => {
   app.get(`/${config.apiPrefix}/v1/hardware/baseboard`, (req, res) => {
     if (utils.authenticate(req.headers.authorization)) {
       const worker = new Worker(path.join(__dirname, '..', '..', '..', 'workers', 'v1', 'systeminformation', 'hardware', 'baseboard.js'));
-      subscribe(res, worker);
+      utils.registerWorker(res, worker);
     } else {
       utils.badRequest(req, "❌ 認證失敗");
     }
@@ -49,7 +40,7 @@ module.exports.register = (app) => {
   app.get(`/${config.apiPrefix}/v1/hardware/chassis`, (req, res) => {
     if (utils.authenticate(req.headers.authorization)) {
       const worker = new Worker(path.join(__dirname, '..', '..', '..', 'workers', 'v1', 'systeminformation', 'hardware', 'chassis.js'));
-      subscribe(res, worker);
+      utils.registerWorker(res, worker);
     } else {
       utils.badRequest(req, "❌ 認證失敗");
     }
