79ce0fdeb5ff4feaf2adda1caf389c1a7a5a4ba7
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-12 11:15:32 +0800

    add /api/v1/general/time api

--- model/api/v1/systeminformation/general.js
+++ model/api/v1/systeminformation/general.js
@@ -13,4 +13,12 @@ module.exports.register = (app) => {
       utils.badRequest(req, "❌ 認證失敗");
     }
   });
+  app.get(`/${config.apiPrefix}/v1/general/time`, (req, res) => {
+    if (utils.authenticate(req.headers.authorization)) {
+      const worker = new Worker(path.join(__dirname, '..', '..', '..', 'workers', 'v1', 'systeminformation', 'general', 'time.js'));
+      utils.registerWorker(res, worker);
+    } else {
+      utils.badRequest(req, "❌ 認證失敗");
+    }
+  });
 }
