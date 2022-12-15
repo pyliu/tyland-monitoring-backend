d58a9e885374e88df2dd7cc9cf47d9d433f87647
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-15 13:57:26 +0800

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: 'tyland-monitoring-backend',
+    exec_mode: 'cluster',
+    instances: -1,
+    script: './server.js',
+    out_file: './logs/backend_out.log',
+    error_file: './logs/backend_err.log',
+    cron_restart: '0 7 * * *',
+    time: true,
+    watch: true,
+    ignore_watch: ['[/\\]./', 'node_modules', '*.bat', '.git', '.vscode', 'thunder-tests', 'assets', 'logs'],
+    max_memory_restart: '256M',
+    env: {
+      NODE_ENV: 'production'
+    },
+    wait_ready: true,
+    restart_delay: 10000
+  }]
+}
