562dcc08c804da9328afae55301dca2fefaec989
pyliu <3634168+pyliu@users.noreply.github.com>
2023-01-16 02:11:25 +0800

    Update index.js
    use common payload structure.

--- model/workers/v1/l05/index.js
+++ model/workers/v1/l05/index.js
@@ -18,29 +18,34 @@ parentPort.on("message", async (params) => {
   try {
     let message = "👌 繼續執行取得 L05 綜合分析資訊 ... ";
     config.isDev && console.log(__basename, message);
+    const payload = {
+      loading: undefined,
+      path: config.l05BuildtsPath,
+      logs: undefined
+    };
     // #1 check if the process is running
     const [ { proc, pid, pids, cpu, mem } ] = await si.processLoad(config.l05ProcessName);
+    payload.loading = { proc, pid, pids, cpu, mem };
     if (!parseInt(pid)) {
       message = '同步程式尚未執行';
       response.statusCode = config.statusCode.FAIL_NOT_RUNNING;
-      response.payload = { proc, pid, pids, cpu, mem };
     } else if (!pathExistsSync(config.l05BuildtsPath)) {
       // #2 check if the sync dir exists
       message = '找不到同步異動檔案存放資料夾';
       response.statusCode = config.statusCode.FAIL_NOT_EXISTS;
-      response.payload = config.l05BuildtsPath;
     } else {
       // #1, #2 are ok, #3 getting the latest 10(default) logs
       const limit = parseInt(params.limit) || 10;
       const db = require(path.join(config.rootPath, "model", "l05MySQL"));
-      const [result, dontcareFields] = await db.query(`SELECT * FROM qrysublog ORDER BY findate desc, qryid desc LIMIT ${limit}`) ;
+      const [logs, dontcareFields] = await db.query(`SELECT * FROM qrysublog ORDER BY findate desc, qryid desc LIMIT ${limit}`) ;
       message = '🟢 L05服務正常運作中';
       /**
        * put retrived logs into payload
        */
-      response.payload = result;
+      payload.logs = logs;
       response.statusCode = config.statusCode.SUCCESS;
     }
+    response.payload = payload;
     response.message = message;
     config.isDev && console.log(__basename, response);
     // }
